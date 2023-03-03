b8c75807ac596775acb44255acbb442fd541aa0f
mzabrisk <mattzabriskie@Matts-MacBook-Air.local>
2023-03-03 10:43:20 -0700

    updating

---
+++ queries.sql
@@ -0,0 +1,45 @@
+CREATE TABLE doctors (
+ id INT PRIMARY KEY NOT NULL,
+ specialty TEXT,
+ taking_patients BOOLEAN
+);
+CREATE TABLE patients (
+ id INT NOT NULL,
+ doctor_id INT NOT NULL,
+ health_status TEXT,
+ PRIMARY KEY (id, doctor_id),
+ FOREIGN KEY (doctor_id) REFERENCES doctors (id)
+);
+
+INSERT INTO doctors(id, specialty, taking_patients)
+VALUES
+(1, 'cardiology', TRUE),
+(2, 'orthopedics', FALSE),
+(3, 'pediatrics', TRUE);
+INSERT INTO patients (id, doctor_id, health_status)
+VALUES
+(1, 2, 'healthy'),
+(2, 3, 'sick'),
+(3, 2, 'sick'),
+(4, 1, 'healthy'),
+(5, 1, 'sick');
+
+-- Read Tables
+SELECT * FROM doctors
+
+SELECT * FRoM patients
+
+
+-- Update rows
+UPDATE doctors
+SET taking_patients = FALSE
+WHERE id = 1;
+UPDATE patients
+SET health_status = 'healthy'
+WHERE id = 1;
+
+
+-- Delete row
+DELETE FROM patients
+WHERE id = 1;
+
